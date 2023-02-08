39343c9fc8d7d0e8b3787b5ac397c1631b3e5460
OlegStankevich1973 <stankeviz@mail.ru>
2023-02-08 15:56:32 +0300

    Добавил решение DZ7

---
+++ DZ7/Main.java
@@ -0,0 +1,13 @@
+package DZ7;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        Presenter p = new Presenter();
+        p.button();
+
+    }
+
+}
